4d1d7c3ad23a7b09664cb60c979d173dd7104c09
Avishay Cohen <avishay.c>
2020-07-20 20:49:00 +0300

    solution for task 1

---
+++ 11-Component-assignments-1/src/main.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { useState } from 'react';
+
+const App = () => {
+    const [typing, setTyping] = useState("Type something ...");
+    const style = { 
+        paddingLeft: "8px",
+        paddingTop: "6px",
+        paddingBottom: "6px",
+        marginBottom: "6px"
+     }
+
+    function handleChanges(event) {
+        setTyping(event.target.value);
+    }
+
+    return (
+        <div>
+            <input
+                type="text"
+                value={typing}
+                onChange={handleChanges}
+                style={style}
+            />
+            <div></div>
+            <input
+                type="text"
+                value={typing}
+                onChange={handleChanges}
+                style={style}
+            />
+            <div></div>
+            <input
+                type="text"
+                value={typing}
+                onChange={handleChanges}
+                style={style}
+            />
+            <div></div>
+            <input
+                type="text"
+                value={typing}
+                onChange={handleChanges}
+                style={style}
+            />
+            <div></div>
+            <input
+                type="text"
+                value={typing}
+                onChange={handleChanges}
+                style={style}
+            />
+        </div>
+    )
+};
+
+
+// main.js
+const root = document.querySelector('main');
+ReactDOM.render(<App />, root);
