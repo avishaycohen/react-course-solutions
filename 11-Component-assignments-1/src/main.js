f4e07b1d944929843e458569ee7e7ea2274e10fb
Avishay Cohen <avishay.c>
2020-07-20 23:21:42 +0300

    fixed lab1

--- 11-Component-assignments-1/src/main.js
+++ 11-Component-assignments-1/src/main.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom';
 import { useState } from 'react';
 
 const App = () => {
+    const loopArray = [1,2,3,4,5];
     const [typing, setTyping] = useState("Type something ...");
-    const style = { 
+    const style = {
         paddingLeft: "8px",
         paddingTop: "6px",
         paddingBottom: "6px",
         marginBottom: "6px"
-     }
+    }
 
     function handleChanges(event) {
         setTyping(event.target.value);
@@ -17,44 +18,21 @@ const App = () => {
 
     return (
         <div>
-            <input
-                type="text"
-                value={typing}
-                onChange={handleChanges}
-                style={style}
-            />
-            <div></div>
-            <input
-                type="text"
-                value={typing}
-                onChange={handleChanges}
-                style={style}
-            />
-            <div></div>
-            <input
-                type="text"
-                value={typing}
-                onChange={handleChanges}
-                style={style}
-            />
-            <div></div>
-            <input
-                type="text"
-                value={typing}
-                onChange={handleChanges}
-                style={style}
-            />
-            <div></div>
-            <input
-                type="text"
-                value={typing}
-                onChange={handleChanges}
-                style={style}
-            />
+            {
+                loopArray.map(() => (
+                    <div>
+                        <input
+                            type="text"
+                            value={typing}
+                            onChange={handleChanges}
+                            style={style}
+                        />
+                    </div>
+                ))
+            }
         </div>
     )
 };
-
 
 // main.js
 const root = document.querySelector('main');
