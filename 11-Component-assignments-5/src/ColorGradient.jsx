e4b97d982a929f915f10ba1fb9bb3924b3f377b6
ynonp <ynonperek@gmail.com>
2020-07-22 07:17:53 +0300

    add comments on ex5 and 16-1

--- 11-Component-assignments-5/src/ColorGradient.jsx
+++ 11-Component-assignments-5/src/ColorGradient.jsx
@@ -9,20 +9,46 @@ export default function ColorGradient(props) {
     // generate series of colors
     let colorsArray = tColor.monochromatic(10);
 
+  // Since all you do is return the result of map,
+  // you can alternatively write:
+  // return colorsArray.map(...)
+  //
+  // So a nice re-structuring of this code is to define a helper function
+  function renderColorBox(color) {
     return (
-        <>
-            {
-            colorsArray.map(
-                color => <div
-                            key={color.toHexString()}
-                            className="boxed"
-                            style={{
-                                backgroundColor: color.toHexString(),
-                                width: "100px",
-                                height: "100px",
-                            }}
-                        />
-            )}
-        </>
-    )
+      <div
+        key={color.toHexString()}
+        className="boxed"
+        style={{
+          backgroundColor: color.toHexString(),
+          width: "100px",
+          height: "100px",
+        }}
+      />
+    );
+  }
+
+  // And then use:
+  return colorsArray.map(renderColorBox);
+
+  // Nicer still is to accept this function from external code and use a default
+  // value. See my example here:
+  // https://www.tocode.co.il/blog/2020-07-react-render-props
+
+    // return (
+    //     <>
+    //         {
+    //         colorsArray.map(
+    //             color => <div
+    //                         key={color.toHexString()}
+    //                         className="boxed"
+    //                         style={{
+    //                             backgroundColor: color.toHexString(),
+    //                             width: "100px",
+    //                             height: "100px",
+    //                         }}
+    //                     />
+    //         )}
+    //     </>
+    // )
 }
