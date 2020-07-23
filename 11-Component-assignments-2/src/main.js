ab6cd9f1179ec130928204c3723e61c79694ffa7
avishayc <avishay.cohen@gmail.com>
2020-07-23 14:20:04 +0300

    refactored lab 2

--- 11-Component-assignments-2/src/main.js
+++ 11-Component-assignments-2/src/main.js
@@ -2,58 +2,40 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { useState } from 'react';
 
-const App = () => {
-    const [seconds, setSeconds] = useState(0)
-    const [minutes, setMinutes] = useState(0)
-    const [hours, setHours] = useState(0)
-
-    const style = {
+const styles = {
+    input: {
         paddingLeft: "8px",
         paddingTop: "6px",
         paddingBottom: "6px",
-        marginBottom: "6px"
+        marginBottom: "6px",
     }
+}
 
-    function handleChanges(event, timeLetter) {
+const factors = [1, 60, 3600]
+
+function App() {
+    const [seconds, setSeconds] = useState(0)
+
+    function handleChanges(event, factor) {
         const newValue = Number(event.target.value)
-        if (timeLetter === 's') {
-            setSeconds(newValue);
-            setMinutes(newValue / 60);
-            setHours(newValue / 60 / 60);
-        } else if (timeLetter === 'm') {
-            setSeconds(newValue * 60);
-            setMinutes(newValue);
-            setHours(newValue / 60);
-        } else {
-            setSeconds(newValue * 60 * 60);
-            setMinutes(newValue * 60);
-            setHours(newValue);
-        }
+        setSeconds(newValue * factor);
     }
 
     return (
-        <div>
-            <input
-                type="number"
-                value={seconds}
-                onChange={(e) => handleChanges(e, 's')}
-                style={style}
-            />
-            <div></div>
-            <input
-                type="number"
-                value={minutes}
-                onChange={(e) => handleChanges(e, 'm')}
-                style={style}
-            />
-            <div></div>
-            <input
-                type="number"
-                value={hours}
-                onChange={(e) => handleChanges(e, 'h')}
-                style={style}
-            />
-        </div>
+        <>
+        {
+            factors.map(
+                factor =>
+                    <input
+                        key={factor}
+                        type="number"
+                        value={seconds / factor}
+                        onChange={(e) => handleChanges(e, factor)}
+                        style={styles.input}
+                    />
+            )
+        }
+        </>
     )
 };
 
